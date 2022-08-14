ab037523762cbb2434dd8e077d4fa271b982f681
derlinchen <derlin_nj@163.com>
2022-08-14 10:33:05 +0800

    update

--- src/components/main/main.vue
+++ src/components/main/main.vue
@@ -1,6 +1,7 @@
 <template>
     <Layout style="height: 100%" class="main">
-        <Sider hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed" class="left-sider" :style="{overflow: 'hidden'}">
+        <Sider hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed" class="left-sider"
+            :style="{ overflow: 'hidden' }">
             <side-menu accordion ref="sideMenu" :active-name="$route.name" :collapsed="collapsed"
                 @on-select="turnToPage" :menu-list="menuList">
                 <div class="logo-con">
@@ -12,7 +13,8 @@
 
         <Layout>
             <Header class="header-con">
-
+                <header-bar :collapsed="collapsed" @on-coll-change="handleCollapsedChange">
+                </header-bar>
             </Header>
 
             <Content class="main-content-con">
@@ -29,6 +31,8 @@ import './main.less'
 import { getMenuListFromLocalstorage } from '@/libs/util'
 
 import SideMenu from './components/side-menu'
+import HeaderBar from './components/header-bar'
+
 import minLogo from '@/assets/images/logo-min.jpg'
 import maxLogo from '@/assets/images/logo.jpg'
 
@@ -36,7 +40,8 @@ import maxLogo from '@/assets/images/logo.jpg'
 export default {
     name: 'Main',
     components: {
-        SideMenu
+        SideMenu,
+        HeaderBar
     },
 
     data() {
@@ -55,6 +60,10 @@ export default {
 
     methods: {
         turnToPage(route) {
+        },
+        handleCollapsedChange(state) {
+            console.log(state)
+            this.collapsed = state
         }
     }
 }
