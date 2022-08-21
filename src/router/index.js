c0bd2708e8159521e7cfacea9d7e03da034e45c5
derlinchen <derlin_nj@163.com>
2022-08-21 21:24:24 +0800

    loading处理

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from '@/router/routes'
+import ViewUIPlus from 'view-ui-plus'
 
 const router = createRouter({
   routes,
@@ -8,5 +9,15 @@ const router = createRouter({
     return { top: 0 }
   }
 })
+
+
+router.beforeEach((to, from, next) => {
+  ViewUIPlus.LoadingBar.start();
+  next();
+});
+
+router.afterEach(route => {
+  ViewUIPlus.LoadingBar.finish();
+});
 
 export default router
