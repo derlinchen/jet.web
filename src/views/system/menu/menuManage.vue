b25b262df871847a74c829c3499aff878173fab0
derlinchen <derlin_nj@163.com>
2022-08-28 20:31:21 +0800

    update

--- src/views/system/menu/menuManage.vue
+++ src/views/system/menu/menuManage.vue
@@ -1,27 +1,27 @@
 <template>
     <Card>
         <div>
-            <Form inline :label-width="100">
-                <FormItem label="菜单编号：">
-                    <Input clearable placeholder="输入菜单编号" v-model="search.menuCode" />
+            <Form inline :label-width="100" style="text-align: right;">
+                <FormItem>
+                    <Space wrap>
+                        <Button @click="refreshMenu" icon="md-refresh" type="primary">
+                            刷新
+                        </Button>
+                        <Button @click="showAddMenu" icon="md-add" type="primary">
+                            添加
+                        </Button>
+                        <Button @click="showEditMenu" icon="md-create" type="primary">
+                             修改
+                        </Button>
+                        <Button @click="deleteMenu" icon="md-trash" type="error">
+                            删除
+                        </Button>
+                    </Space>
                 </FormItem>
-                <FormItem label="菜单名称：">
-                    <Input clearable placeholder="输入菜单名称" v-model="search.menuName" />
-                </FormItem>
-
-                <Space wrap>
-                    <Button @click="searchMenu" icon="md-search" type="primary">
-                        查询
-                    </Button>
-                    <Button @click="resetSearch" icon="md-refresh" type="primary">
-                        重置
-                    </Button>
-                </Space>
-
             </Form>
         </div>
 
-        <Table border :loading="loading" :columns="columns" :data="tableData">
+        <Table border :loading="loading" :columns="menuColumns" :data="menuTableData">
         </Table>
         <!-- <div style="margin-top:10px">
             <Page :total="page.total" :page-size="page.pageSize" show-total show-elevator show-sizer
@@ -37,17 +37,51 @@ export default {
     name: 'menuManage',
     data() {
         return {
-            search: {},
-            loading: false
+            search: {
+                pageNo: 1,
+                total: 0,
+                pageSize: 10,
+                item: {}
+            },
+            loading: false,
+            menuColumns: [
+                {
+                    title: '菜单标题',
+                    key: 'title'
+                },
+                {
+                    title: '路由名称',
+                    key: 'name'
+                },
+                {
+                    title: '菜单图标',
+                    key: 'icon'
+                },
+                {
+                    title: '菜单URL',
+                    key: 'url'
+                },
+                {
+                    title: '创建人',
+                    key: 'createUserName'
+                },
+                {
+                    title: '创建时间',
+                    key: 'createDate'
+                }
+            ],
+            menuTableData:[]
         }
     },
     methods: {
-        searchMenu() {
+        getMenuList() {
 
         },
-        resetSearch() {
-            this.search = {}
-            this.searchMenu()
+        showAddMenu() {
+
+        },
+        showEditMenu() {
+
         }
     }
 }
